93e3237c123ee5940c3d910be19e668912bb3970
Patrick Shampine <pshampine@gunnjerkens.com>
2014-05-06 17:48:05 -0700

    Renaming functions to prevent namespace issues w/ other gj-plugins

--- gj-custom-login.php
+++ gj-custom-login.php
@@ -8,13 +8,14 @@ Author: Gunn|Jerkens
 Author URI: http://gunnjerkens.com
 */
 
-require_once('gj-user-approve-functions.php');
+require_once('gj-custom-login-functions.php');
+
 add_action('admin_menu', 'gj_admin_actions');
 
-function gj_admin_actions() {
+function gj_custom_login_admin_actions() {
   add_menu_page( "GJ Custom Login", "GJ Custom Login", 'administrator', "gj_custom_login", "gj_admin_options" );
 }
 
-function gj_admin_options() {
+function gj_custom_login_admin_options() {
   include ('admin/gj-user-approve-options.php');
 }
